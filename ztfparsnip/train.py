8d73fc463c12691ff7d99fd0e5fe4678c83fbceb
simeonreusch <simeon.reusch@desy.de>
2022-12-19 17:15:31 +0100

    make parsnip actually run

--- ztfparsnip/train.py
+++ ztfparsnip/train.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
-import logging, os, subprocess
+import logging, os, subprocess, time
 
-import h5py  # type: ignore
 import lcdata  # type: ignore
 import pandas as pd  # type: ignore
+import numpy as np
+
+import parsnip
 
 
 class Train:
     def __init__(self, path: str):
         self.training_path = path
-
-        # with h5py.File(self.training_path, "r") as f:
-        #     keys = list(f.keys())
-        #     meta = list(f["metadata"])
+        self.logger = logging.getLogger(__name__)
 
         meta_df = lcdata.read_hdf5(self.training_path, in_memory=False).meta.to_pandas()
 
@@ -40,42 +39,91 @@ class Train:
         )
 
         self.meta = meta_df
+        self.print_statistics()
 
     def print_statistics(self):
         """Get some statistics on the training set"""
         n_classes = len(unique_class := self.meta["class"].unique())
-        print(f"There are {n_classes} different classes, these are:")
+        self.logger.info(
+            f"There are {n_classes} different classes in the training data, these are:"
+        )
         for cl in unique_class:
-            print(cl)
-        print("--------")
+            self.logger.info(cl)
+        self.logger.info("--------")
         n_parent_id = len(unique_parent := self.meta["parent_id"].unique())
-        print(
+        self.logger.info(
             f"There are {n_parent_id} parent objects. From these, {len(self.meta)} lightcurves have been created."
         )
 
     def run(self, threads: int = 10, outfile: str | None = None):
         """Run the actual train command"""
-        model_path = os.path.basename(self.training_path)
-        print(model_path)
-        # p = subprocess.Popen(
-        #     f"parsnip_train --split_train_test --threads={threads}  model/model-1-50pct-sn-mw.h5 data/model-1-50-sn.h5",
-        #     stdout=subprocess.PIPE,
-        #     shell=True,
-        # )
-        # print(p.communicate())
+        if outfile is None:
+            model_dir = os.path.dirname(self.training_path)
+            outfile = os.path.join(
+                model_dir,
+                os.path.basename(self.training_path).split(".")[0] + "_model.hd5",
+            )
+        # cmd = f"parsnip_train --split_train_test --threads={threads}  {outfile} {self.training_path}"
+
+        start_time = time.time()
+
+        parser = parsnip.build_default_argparse("Run Parsnip")
+        args = vars(parser.parse_args())
+        args["overwrite"] = True
+        args["max_epochs"] = 1000
+
+        dataset = parsnip.load_datasets(
+            [self.training_path],
+            require_redshift=True,
+        )
+        bands = parsnip.get_bands(dataset)
+
+        model = parsnip.ParsnipModel(
+            outfile,
+            bands,
+            device="cuda",
+            threads=8,
+            settings=args,
+            ignore_unknown_settings=True,
+        )
+        dataset = model.preprocess(dataset)
+
+        # do own test train validation split here!!!!
+        train_dataset, test_dataset = parsnip.split_train_test(dataset)
+        model.fit(
+            train_dataset, test_dataset=test_dataset, max_epochs=args["max_epochs"]
+        )
+
+        rounds = int(np.ceil(25000 / len(train_dataset)))
+
+        train_score = model.score(train_dataset, rounds=rounds)
+        test_score = model.score(test_dataset, rounds=10 * rounds)
+
+        end_time = time.time()
+
+        # Time taken in minutes
+        elapsed_time = (end_time - start_time) / 60.0
+
+        with open("./parsnip_results.log", "a") as f:
+            print(
+                f"{model_path} {model.epoch} {elapsed_time:.2f} {train_score:.4f} "
+                f"{test_score:.4f}",
+                file=f,
+            )
+
+    # noisification_paramdict = {
+    #     "max_redshift_delta": 0.1,
+    #     "classes": ["tde", "slsn", "baratheon", "snia"],
+    #     "redshift_dist": "cubic",
+    #     "k_scale": 1.25,
+    #     "phase_limit": {"long_lived": [-50, 200], "short_lived": [-30, 50]},
+    #     "no_k_classes": ["tde", "slsn"],
+    #     "sn_cuts": {"n_det": 5, "sn_threshold": 5},
+    #     "k_correction": None,
+    #     "augmentation": {
+    #         "mode": "per_class",
 
 
-# noisification_paramdict = {
-#     "max_redshift_delta": 0.1,
-#     "classes": ["tde", "slsn", "baratheon", "snia"],
-#     "redshift_dist": "cubic",
-#     "k_scale": 1.25,
-#     "phase_limit": {"long_lived": [-50, 200], "short_lived": [-30, 50]},
-#     "no_k_classes": ["tde", "slsn"],
-#     "sn_cuts": {"n_det": 5, "sn_threshold": 5},
-#     "k_correction": None,
-#     "augmentation": {
-#         "mode": "per_class",
 #         "multiplier": {"snia": 10, "slsn": 100, "tde": 100, "baratheon": 100},
 #     },
 #     "verification_percentage": 0.1,
