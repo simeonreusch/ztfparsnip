c1f821ed0accd299172ed816fa68cf3b946668d2
simeonreusch <simeon.reusch@desy.de>
2022-12-19 16:19:44 +0100

    use lcdata to handle hdf interfacing

--- ztfparsnip/train.py
+++ ztfparsnip/train.py
@@ -3,30 +3,43 @@
 import logging, os, subprocess
 
 import h5py  # type: ignore
+import lcdata  # type: ignore
 import pandas as pd  # type: ignore
 
 
 class Train:
     def __init__(self, path: str):
-        self.path = path
+        self.training_path = path
 
-        with h5py.File(self.path, "r") as f:
-            keys = list(f.keys())
-            meta = list(f["metadata"])
-        meta_dict = {}
-        for entry in meta:
-            meta_dict.update(
-                {
-                    entry[0].decode("ascii"): {
-                        "z": entry[4],
-                        "parent_id": entry[5].decode("ascii"),
-                        "parent_z": float(entry[10].decode("ascii")),
-                        "class": entry[9].decode("ascii"),
-                    },
-                }
-            )
+        # with h5py.File(self.training_path, "r") as f:
+        #     keys = list(f.keys())
+        #     meta = list(f["metadata"])
 
-        self.meta = pd.DataFrame.from_dict(meta_dict, orient="index")
+        meta_df = lcdata.read_hdf5(self.training_path, in_memory=False).meta.to_pandas()
+
+        meta_df.drop(
+            columns=[
+                col
+                for col in list(meta_df.keys())
+                if col not in ["object_id", "redshift", "name", "bts_class", "bts_z"]
+            ],
+            inplace=True,
+        )
+        meta_df["name"] = meta_df["name"].str.decode("utf-8")
+        meta_df["bts_class"] = meta_df["bts_class"].str.decode("utf-8")
+        meta_df["bts_z"] = meta_df["bts_z"].str.decode("utf-8").astype(float)
+
+        meta_df.rename(
+            columns={
+                "name": "parent_id",
+                "bts_class": "class",
+                "redshift": "z",
+                "bts_z": "parent_z",
+            },
+            inplace=True,
+        )
+
+        self.meta = meta_df
 
     def print_statistics(self):
         """Get some statistics on the training set"""
@@ -40,8 +53,16 @@ class Train:
             f"There are {n_parent_id} parent objects. From these, {len(self.meta)} lightcurves have been created."
         )
 
-    def run(self):
+    def run(self, threads: int = 10, outfile: str | None = None):
         """Run the actual train command"""
+        model_path = os.path.basename(self.training_path)
+        print(model_path)
+        # p = subprocess.Popen(
+        #     f"parsnip_train --split_train_test --threads={threads}  model/model-1-50pct-sn-mw.h5 data/model-1-50-sn.h5",
+        #     stdout=subprocess.PIPE,
+        #     shell=True,
+        # )
+        # print(p.communicate())
 
 
 # noisification_paramdict = {
