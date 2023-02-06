4aa25198d02126e33a68738c4b3237f59d70798f
simeonreusch <simeon.reusch@desy.de>
2023-02-06 16:08:26 +0100

    adding tqdm, bts classification

--- ztfparsnip/create.py
+++ ztfparsnip/create.py
@@ -4,6 +4,8 @@
 
 import os, numpy, logging
 
+from tqdm import tqdm
+
 from ztfparsnip import io
 from ztfparsnip.noisify import noisify
 
@@ -29,7 +31,6 @@ class CreateLightcurves(object):
 
         self.ztfids = io.get_all_ztfids(lc_dir=self.lc_dir)
         self.config = io.load_config()
-        self.get_simple_class("TDE")
 
     def get_simple_class(self, bts_class: str) -> str:
         """
@@ -37,8 +38,9 @@ class CreateLightcurves(object):
         """
         for key, val in self.config["simpleclasses"].items():
             for entry in self.config["simpleclasses"][key]:
-                if bts_class == entry:
+                if bts_class == entry or bts_class == f"SN {entry}":
                     return key
+        return "unclass"
 
     def get_lightcurves(self):
         """
@@ -48,7 +50,8 @@ class CreateLightcurves(object):
             lc = io.get_ztfid_dataframe(ztfid=ztfid, lc_dir=self.lc_dir)
             header = io.get_ztfid_header(ztfid=ztfid, lc_dir=self.lc_dir)
             bts_class = header.get("bts_class")
-            print(bts_class)
+            simple_class = self.get_simple_class(bts_class)
+            header["simple_class"] = simple_class
             yield lc, header
 
     def noisify_sample(self):
