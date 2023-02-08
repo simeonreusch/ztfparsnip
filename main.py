a2db762c6cfb859e3bfdc670be22acc86e91aba2
simeonreusch <simeon.reusch@desy.de>
2023-02-08 12:56:45 +0100

    added main

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import logging, os
+import argparse
+
+from ztfparsnip.train import Train
+
+
+def run():
+    logging.basicConfig(level=logging.INFO)
+    _parser = argparse.ArgumentParser(description="Create my bills")
+
+    _parser.add_argument(
+        "file",
+        type=str,
+        help="Provide a training sample filepath",
+    )
+    args = _parser.parse_args()
+
+    t = Train(args.file, seed=3)
+    t.run()
+
+
+if __name__ == "__main__":
+    run()
