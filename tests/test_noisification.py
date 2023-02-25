0695394156c1eb02621db363c06ca22de0b51c95
simeonreusch <simeon.reusch@desy.de>
2023-02-25 10:50:12 +0100

    added first test

---
+++ tests/test_noisification.py
@@ -0,0 +1,30 @@
+import logging
+import unittest
+
+from ztfparsnip.create import CreateLightcurves
+
+logging.getLogger("ztfparsnip.create").setLevel(logging.DEBUG)
+
+
+class TestQuery(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+    def test_noisification(self):
+        self.logger.info("\n\n Generating noisified lightcurves\n")
+
+        weights = {"sn_ia": 5, "sn_other": 5, "tde": 5}
+
+        sample = CreateLightcurves(
+            output_format="parsnip",
+            classkey="simpleclasses",
+            weights=weights,
+            train_dir="train",
+            plot_dir="plot",
+            seed=100,
+            phase_lim=True,
+            k_corr=True,
+        )
+        sample.select()
+        sample.create(plot_debug=True)
