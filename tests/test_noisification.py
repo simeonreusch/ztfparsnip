f27e6490fc61c5aa25aec8f28f5af69c1ac3fe97
simeonreusch <simeon.reusch@desy.de>
2023-02-25 11:06:03 +0100

    adding csv export test

--- tests/test_noisification.py
+++ tests/test_noisification.py
@@ -6,12 +6,12 @@ from ztfparsnip.create import CreateLightcurves
 logging.getLogger("ztfparsnip.create").setLevel(logging.DEBUG)
 
 
-class TestQuery(unittest.TestCase):
+class TestNoisification(unittest.TestCase):
     def setUp(self):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
 
-    def test_noisification(self):
+    def test_noisification_parsnip(self):
         self.logger.info("\n\n Generating noisified lightcurves\n")
 
         weights = {"sn_ia": 5, "sn_other": 5, "tde": 5}
@@ -29,3 +29,22 @@ class TestQuery(unittest.TestCase):
         )
         sample.select()
         sample.create(plot_debug=True)
+
+    def test_noisification_csv(self):
+        self.logger.info("\n\n Generating noisified lightcurves\n")
+
+        weights = {"sn_ia": 5, "sn_other": 5, "tde": 5}
+
+        sample = CreateLightcurves(
+            output_format="ztfnuclear",
+            classkey="simpleclasses",
+            weights=weights,
+            train_dir="train",
+            plot_dir="plot",
+            seed=100,
+            phase_lim=True,
+            k_corr=True,
+            test=True,
+        )
+        sample.select()
+        sample.create(plot_debug=False)
